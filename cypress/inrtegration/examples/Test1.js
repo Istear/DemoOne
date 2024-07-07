69d57073cc9652a412dcbdee2f9ee4a622df37cf
Istear <istearahmed@gmail.com>
2024-07-07 13:24:44 +0600

    #

--- cypress/inrtegration/examples/Test1.js
+++ cypress/inrtegration/examples/Test1.js
@@ -1,5 +1,13 @@
+/// <reference types="Cypress" />
 describe ('Basic first tests',()=>{
-    it('Navigate to the website',()=>{
+    it('Navigate to the website and locate the search box',()=>{
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.get('.search-keyword')
+    })
+    it('Search Cauliflower in the searchbox and verify the search result',()=>{
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.get('.search-keyword').type('Cauliflower');
+        cy.wait(2000);
+        cy.get('img[alt="Cauliflower - 1 Kg"]').should("be.visible");
     })
 })
